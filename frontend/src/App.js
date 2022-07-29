2b251eb4bb497c445075d515db7bc251e1c7bdd1
Tarek Noaman <progtarek@gmail.com>
2022-07-29 22:09:16 +0200

    [FE] | (Layout) Add initial layout setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Header, Footer } from "./components";
+
+// Lazy Pages
+const Home = React.lazy(() =>
+  import(/* webpackPrefetch: true  */ "./pages/Home.page")
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Fragment>
   );
 }
 
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+import Footer from "./footer/Footer.component";
+import Header from "./header/Header.component";
+
+export { Footer, Header };
